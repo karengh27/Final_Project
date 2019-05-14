f26f6b05415a5cac1d641e9fe52769cb85a6d834
Karen Ghazal <ghazal.karen@gmail.com>
2019-05-14 15:50:04 -0400

    first commit

---
+++ Term_project.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+import java.util.Scanner;
+import org.math.plot.*;
+import org.math.plot.plotObjects.*;
+import java.util.Arrays;
+
+public class Term_project
+{
+	public static void main(String[] args)
+    {
+		double percent = 0;
+		int r = 0;
+		
+		System.out.println("Enter the number of rounds:                ");
+		r = keyboard.nextInt();
+  
+		System.out.println("Enter the percentage of miscomunication (between 0 and 100):                ");
+		percent = keyboard.nextDouble();
+		
+		for(int i=0; i < r; i++)
+		{
+			
+		}
+	}
+	public static int Cheater(int a)
+	{
+		a = 0;
+		return a;
+	}
+	public static int Random(int b)
+	{
+		b = Math.round(Math.random());
+		return b;
+	}
+	public static int Copycat(int c, int n);
+	{
+		c = n;
+		return c;
+	}
+	public static int Detective(int d, int n);
+	{
+		
+	}
+}
